a94188424b0556a302a6435e4ff5abb7067058dd
Anurag Dogra <anuragdogra.2192@gmail.com>
2023-08-09 21:58:29 +0200

    Added the split function and mlflow.sklearn.autolog()

--- src/model/train.py
+++ src/model/train.py
@@ -1,18 +1,21 @@
 # Import libraries
 
 import argparse
+import mlflow
+import mlflow.sklearn
 import glob
 import os
 
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    # mlflow.sklearn.autolog() requires mlflow 1.11.0 or above.
+    mlflow.sklearn.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +37,10 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X = df.drop('Y', axis=1).values
+    y = df['Y'].values
+    train_test_split(X, y, test_size=0.30, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
